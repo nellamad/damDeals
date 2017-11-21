c52c9d0341e5c168ff44684f301300a0ccc13363
nellamad <allenqdam@gmail.com>
2017-11-21 00:30:24 -0500

    Revert "Email improvements"
    This reverts commit db74f25a4cbb3eea0fcefb4405bb28563eb00d3f.

--- damDeals.py
+++ damDeals.py
@@ -4,13 +4,14 @@
 
 import urllib.request
 import shutil
-import os
 import csv
 import re
 from xml.dom import minidom
 import filecmp
 import config
-from damEmail import send_deals
+import smtplib
+from email.message import EmailMessage
+from pprint import pprint
 
 GOLDBOX_URL = 'https://rssfeeds.s3.amazonaws.com/goldbox'
 GOLDBOX_FILE = 'goldbox.xml'
@@ -66,24 +67,35 @@ def dam_Deals():
 							dealsWriter.writerow([price, title, link])
 							break
 
-	if os.path.getsize(TEMP_DEALS) > 0:
-		try:
-			open(CURATED_DEALS, 'x')
-		except FileExistsError:
-			print("Comparing with old deals...")
+	try:
+		open(CURATED_DEALS, 'x')
+	except FileExistsError:
+		print("Comparing with old deals...")
 
-		if filecmp.cmp(TEMP_DEALS, CURATED_DEALS, False):
-			print("No new deals found...")
-		else:
-			print("New deals found...")
-			shutil.copyfile(TEMP_DEALS, CURATED_DEALS)
-			if EMAIL_ENABLED:
-				# email the deals that we've found, if there are any
-				if os.path.exists(CURATED_DEALS) and os.path.getsize(CURATED_DEALS) > 0:
-					send_deals(open(CURATED_DEALS))
-	else:
+	if filecmp.cmp(TEMP_DEALS, CURATED_DEALS, False):
 		print("No new deals found...")
+	else:
+		shutil.copyfile(TEMP_DEALS, CURATED_DEALS)
+		if EMAIL_ENABLED:
+			send_deals()
+		print("New deals found...")
 
+def send_deals():
 
+	# email the deals that we've found
+	with open(CURATED_DEALS) as deals:
+		message = EmailMessage()
+		message.set_content(deals.read())
+
+	message['Subject'] = 'Your Dam Deals'
+	message['From'] = config.GMAIL_USER
+	message['To'] = config.SUBSCRIBERS
+
+	server = smtplib.SMTP_SSL('smtp.gmail.com')
+	server.login(config.GMAIL_USER, config.GMAIL_PASSWORD)
+	server.send_message(message)
+	server.quit()
+
+	print('Updated list has been sent!')
 
 dam_Deals()
