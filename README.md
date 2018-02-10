ecc6091309c26ea7b16abfd6e91a4c0576656381
nellamad <allenqdam@gmail.com>
2018-02-09 22:02:41 -0500

    Improved usage message

--- README.md
+++ README.md
@@ -27,19 +27,19 @@ schedule_cron.py -c -u foo@bar.com -p p4ssw0rd --hours 1
 Full usage:
 ```
 schedule_cron.py --help
-usage: schedule_cron.py [-h] [--smtp SMTP] [-u USER] [-p PASSWORD] [-c] [-s]
-                        [--hours HOURS | --minutes MINUTES] [-v]
+usage: schedule_cron.py [-h] [-v] [-c] [-s] [--smtp SMTP] [-u USER]
+                        [-p PASSWORD] [--hours HOURS | --minutes MINUTES]
 
 optional arguments:
   -h, --help            show this help message and exit
+  -v, --verbose         increase output verbosity
+  -c, --use_cache       exclude deals that were found during last run
+  -s, --suppress_emails
+                        suppress email sending
   --smtp SMTP           SMTP email server (default:smtp.gmail.com)
   -u USER, --user USER  email user
   -p PASSWORD, --password PASSWORD
                         email password
-  -c, --use_cache       exclude deals that were found during last run
-  -s, --suppress_emails
-                        suppress email sending
   --hours HOURS         schedule a run every number of hours
   --minutes MINUTES     schedule a run every number of minutes
-  -v, --verbose         increase output verbosity
  ```
--- schedule_cron.py
+++ schedule_cron.py
@@ -9,15 +9,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    parser.add_argument('-c', '--use_cache', action='store_true', help='exclude deals that were found during last run')
+    parser.add_argument('-s', '--suppress_emails', action='store_true', help='suppress email sending')
     parser.add_argument('--smtp', default='smtp.gmail.com', help='SMTP email server (default:smtp.gmail.com)')
     parser.add_argument('-u', '--user', help='email user')
     parser.add_argument('-p', '--password', help='email password')
-    parser.add_argument('-c', '--use_cache', action='store_true', help='exclude deals that were found during last run')
-    parser.add_argument('-s', '--suppress_emails', action='store_true', help='suppress email sending')
     schedule_group = parser.add_mutually_exclusive_group()
     schedule_group.add_argument('--hours', type=int, help='schedule a run every number of hours')
     schedule_group.add_argument('--minutes', type=int, help='schedule a run every number of minutes')
-    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
     args = parser.parse_args()
 
     if not args.suppress_emails and (args.user is None or args.password is None):
